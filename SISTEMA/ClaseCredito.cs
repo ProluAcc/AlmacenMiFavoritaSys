e7df224e8afec265253222fc84c4679a257e7929
Christiam Martinez <yummiesranchita@gmail.com>
2026-04-24 20:23:26 -0600

    Clases agregadas

---
+++ SISTEMA/ClaseCredito.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SISTEMA
+{
+    internal class ClaseCredito
+    {
+        private int _id_credito;
+        private string _fecha_inicio;
+        private string _fecha_vencimiento;
+        private double _monto;
+        private int _id_venta;
+    }
+}
---
+++ SISTEMA/ClaseVenta.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SISTEMA
+{
+    internal class ClaseVenta
+    {
+        private int _id_venta;
+        private string _fecha_venta;
+        private string _tipo_venta;
+        private int _id_usuario;
+        private int _id_cliente;
+        private int _cantidad;
+        private double _subtotal;
+        private double _descuento;
+        private double _total;
+        private int _id_producto;
+    }
+}

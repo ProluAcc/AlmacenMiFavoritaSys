1dfec05b13caa253b3c8b373ec1ad71170941bd2
Christiam Martinez <yummiesranchita@gmail.com>
2026-04-24 23:45:18 -0600

    Clase caja añadida

---
+++ SISTEMA/ClaseCaja.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SISTEMA
+{
+    internal class ClaseCaja
+    {
+        private int _id_caja;
+        private string _fecha_apertura;
+        private string _fecha_cierre;
+        private double _saldo_inicial;
+        private double _saldo_final;
+    }
+}

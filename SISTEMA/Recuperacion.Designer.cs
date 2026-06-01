5300220be859d3c8c4d09eb093c5dd56df2245ac
Christiam Martinez <yummiesranchita@gmail.com>
2026-05-31 23:03:16 -0600

    se me habia olvidado editar el de recuperación de contraseña lmao

--- SISTEMA/Recuperacion.Designer.cs
+++ SISTEMA/Recuperacion.Designer.cs
@@ -38,6 +38,12 @@
             textBox1 = new TextBox();
             label2 = new Label();
             label1 = new Label();
+            button3 = new Button();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            label5 = new Label();
+            label6 = new Label();
+            button4 = new Button();
             panel1.SuspendLayout();
             SuspendLayout();
             // 
@@ -45,6 +51,12 @@
             // 
             panel1.BackColor = Color.FromArgb(200, 56, 56);
             panel1.BorderStyle = BorderStyle.FixedSingle;
+            panel1.Controls.Add(button4);
+            panel1.Controls.Add(textBox4);
+            panel1.Controls.Add(textBox5);
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(label6);
+            panel1.Controls.Add(button3);
             panel1.Controls.Add(button2);
             panel1.Controls.Add(button1);
             panel1.Controls.Add(textBox3);
@@ -56,7 +68,7 @@
             panel1.Controls.Add(label1);
             panel1.Location = new Point(12, 12);
             panel1.Name = "panel1";
-            panel1.Size = new Size(388, 324);
+            panel1.Size = new Size(420, 449);
             panel1.TabIndex = 2;
             // 
             // button2
@@ -64,7 +76,7 @@
             button2.BackColor = Color.White;
             button2.Font = new Font("Segoe UI Black", 11F, FontStyle.Bold);
             button2.ForeColor = Color.FromArgb(69, 69, 210);
-            button2.Location = new Point(17, 272);
+            button2.Location = new Point(17, 398);
             button2.Name = "button2";
             button2.Size = new Size(113, 37);
             button2.TabIndex = 8;
@@ -78,7 +90,7 @@
             button1.FlatAppearance.BorderColor = Color.FromArgb(23, 23, 255);
             button1.Font = new Font("Segoe UI Black", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             button1.ForeColor = Color.White;
-            button1.Location = new Point(161, 272);
+            button1.Location = new Point(291, 341);
             button1.Name = "button1";
             button1.Size = new Size(102, 37);
             button1.TabIndex = 7;
@@ -110,9 +122,9 @@
             label4.ForeColor = Color.White;
             label4.Location = new Point(17, 194);
             label4.Name = "label4";
-            label4.Size = new Size(211, 20);
+            label4.Size = new Size(204, 20);
             label4.TabIndex = 4;
-            label4.Text = "Confirmar contraseña nueva:";
+            label4.Text = "Respuesta de Recuperación:";
             // 
             // label3
             // 
@@ -121,9 +133,9 @@
             label3.ForeColor = Color.White;
             label3.Location = new Point(17, 128);
             label3.Name = "label3";
-            label3.Size = new Size(192, 20);
+            label3.Size = new Size(196, 20);
             label3.TabIndex = 3;
-            label3.Text = "Ingrese contraseña nueva:";
+            label3.Text = "Pregunta de Recuperación:";
             // 
             // textBox1
             // 
@@ -141,27 +153,93 @@
             label2.ForeColor = Color.White;
             label2.Location = new Point(17, 65);
             label2.Name = "label2";
-            label2.Size = new Size(123, 20);
+            label2.Size = new Size(116, 20);
             label2.TabIndex = 1;
-            label2.Text = "Ingrese Usuario:";
+            label2.Text = "Ingrese Correo:";
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI Black", 18F, FontStyle.Bold);
             label1.ForeColor = Color.White;
-            label1.Location = new Point(17, 16);
+            label1.Location = new Point(39, 16);
             label1.Name = "label1";
             label1.Size = new Size(343, 32);
             label1.TabIndex = 0;
             label1.Text = "Recuperación de Contraseña";
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(69, 69, 210);
+            button3.FlatAppearance.BorderColor = Color.FromArgb(23, 23, 255);
+            button3.Font = new Font("Segoe UI Black", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button3.ForeColor = Color.White;
+            button3.Location = new Point(247, 79);
+            button3.Name = "button3";
+            button3.Size = new Size(102, 37);
+            button3.TabIndex = 9;
+            button3.Text = "Buscar";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
+            textBox4.ForeColor = SystemColors.ControlDarkDark;
+            textBox4.Location = new Point(20, 346);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(246, 27);
+            textBox4.TabIndex = 13;
+            // 
+            // textBox5
+            // 
+            textBox5.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
+            textBox5.ForeColor = SystemColors.ControlDarkDark;
+            textBox5.Location = new Point(20, 280);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(246, 27);
+            textBox5.TabIndex = 12;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
+            label5.ForeColor = Color.White;
+            label5.Location = new Point(20, 323);
+            label5.Name = "label5";
+            label5.Size = new Size(225, 20);
+            label5.TabIndex = 11;
+            label5.Text = "Confirme su nueva contraseña:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
+            label6.ForeColor = Color.White;
+            label6.Location = new Point(20, 257);
+            label6.Name = "label6";
+            label6.Size = new Size(212, 20);
+            label6.TabIndex = 10;
+            label6.Text = "Ingrese su nueva contraseña:";
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.FromArgb(69, 69, 210);
+            button4.FlatAppearance.BorderColor = Color.FromArgb(23, 23, 255);
+            button4.Font = new Font("Segoe UI Black", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button4.ForeColor = Color.White;
+            button4.Location = new Point(291, 207);
+            button4.Name = "button4";
+            button4.Size = new Size(102, 37);
+            button4.TabIndex = 14;
+            button4.Text = "Comprobar";
+            button4.UseVisualStyleBackColor = false;
             // 
             // Recuperacion
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(255, 223, 96);
-            ClientSize = new Size(413, 345);
+            ClientSize = new Size(437, 473);
             Controls.Add(panel1);
             Name = "Recuperacion";
             Text = "Recuperacion";
@@ -182,5 +260,11 @@
         private TextBox textBox1;
         private Label label2;
         private Label label1;
+        private Button button3;
+        private Button button4;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private Label label5;
+        private Label label6;
     }
 }

ff30f5a5975e0db319ace81aff666288c3608829
Christiam Martinez <yummiesranchita@gmail.com>
2026-08-16 23:43:12 -0600

    Primera versión para restaurar la base de datos

--- SISTEMA/capa_de_presentacion/Mantenimiento.Designer.cs
+++ SISTEMA/capa_de_presentacion/Mantenimiento.Designer.cs
@@ -35,20 +35,26 @@
             txtNombreRespaldo = new TextBox();
             label1 = new Label();
             groupBox2 = new GroupBox();
+            label6 = new Label();
+            txtFechaRespaldo = new TextBox();
+            label5 = new Label();
+            txtRutaRespaldo = new TextBox();
+            label4 = new Label();
             txtRestauracion = new TextBox();
-            btnSeleccionar = new Button();
             btnRestaurar = new Button();
             label3 = new Label();
-            dataGridView1 = new DataGridView();
-            Column1 = new DataGridViewTextBoxColumn();
+            dgvRespaldo = new DataGridView();
             groupBox3 = new GroupBox();
             groupBox4 = new GroupBox();
             btnEliminar = new Button();
             btnDescargar = new Button();
             button7 = new Button();
+            Column1 = new DataGridViewTextBoxColumn();
+            Column3 = new DataGridViewTextBoxColumn();
+            Column2 = new DataGridViewTextBoxColumn();
             groupBox1.SuspendLayout();
             groupBox2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dgvRespaldo).BeginInit();
             groupBox3.SuspendLayout();
             groupBox4.SuspendLayout();
             SuspendLayout();
@@ -59,7 +65,7 @@
             button1.Enabled = false;
             button1.Location = new Point(0, -1);
             button1.Name = "button1";
-            button1.Size = new Size(976, 61);
+            button1.Size = new Size(1189, 61);
             button1.TabIndex = 100;
             button1.UseVisualStyleBackColor = false;
             // 
@@ -83,7 +89,7 @@
             groupBox1.Controls.Add(label1);
             groupBox1.Location = new Point(12, 66);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(385, 109);
+            groupBox1.Size = new Size(455, 109);
             groupBox1.TabIndex = 102;
             groupBox1.TabStop = false;
             groupBox1.Text = "Crear Respaldo";
@@ -93,7 +99,7 @@
             btnRespaldo.BackColor = SystemColors.ControlLight;
             btnRespaldo.Font = new Font("Segoe UI Black", 11F, FontStyle.Bold);
             btnRespaldo.ForeColor = Color.FromArgb(69, 69, 210);
-            btnRespaldo.Location = new Point(176, 51);
+            btnRespaldo.Location = new Point(259, 65);
             btnRespaldo.Name = "btnRespaldo";
             btnRespaldo.Size = new Size(190, 38);
             btnRespaldo.TabIndex = 15;
@@ -104,9 +110,9 @@
             // txtNombreRespaldo
             // 
             txtNombreRespaldo.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
-            txtNombreRespaldo.Location = new Point(176, 18);
+            txtNombreRespaldo.Location = new Point(108, 26);
             txtNombreRespaldo.Name = "txtNombreRespaldo";
-            txtNombreRespaldo.Size = new Size(190, 27);
+            txtNombreRespaldo.Size = new Size(341, 27);
             txtNombreRespaldo.TabIndex = 14;
             // 
             // label1
@@ -116,56 +122,98 @@
             label1.ForeColor = Color.Black;
             label1.Location = new Point(6, 19);
             label1.Name = "label1";
-            label1.Size = new Size(164, 20);
+            label1.Size = new Size(96, 40);
             label1.TabIndex = 4;
-            label1.Text = "Nombre del Respaldo:";
+            label1.Text = "Nombre del \r\nRespaldo:";
             // 
             // groupBox2
             // 
             groupBox2.BackColor = SystemColors.Control;
+            groupBox2.Controls.Add(label6);
+            groupBox2.Controls.Add(txtFechaRespaldo);
+            groupBox2.Controls.Add(label5);
+            groupBox2.Controls.Add(txtRutaRespaldo);
+            groupBox2.Controls.Add(label4);
             groupBox2.Controls.Add(txtRestauracion);
-            groupBox2.Controls.Add(btnSeleccionar);
             groupBox2.Controls.Add(btnRestaurar);
             groupBox2.Controls.Add(label3);
-            groupBox2.Location = new Point(417, 66);
+            groupBox2.Location = new Point(473, 66);
             groupBox2.Name = "groupBox2";
-            groupBox2.Size = new Size(546, 109);
+            groupBox2.Size = new Size(676, 192);
             groupBox2.TabIndex = 103;
             groupBox2.TabStop = false;
             groupBox2.Text = "Restaurar Base de Datos";
             groupBox2.Enter += groupBox2_Enter;
             // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
+            label6.ForeColor = Color.Black;
+            label6.Location = new Point(11, 126);
+            label6.Name = "label6";
+            label6.Size = new Size(74, 40);
+            label6.TabIndex = 19;
+            label6.Text = "Fecha de \r\nCreación:";
+            // 
+            // txtFechaRespaldo
+            // 
+            txtFechaRespaldo.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
+            txtFechaRespaldo.Location = new Point(113, 129);
+            txtFechaRespaldo.Name = "txtFechaRespaldo";
+            txtFechaRespaldo.Size = new Size(394, 27);
+            txtFechaRespaldo.TabIndex = 20;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
+            label5.ForeColor = Color.Black;
+            label5.Location = new Point(12, 74);
+            label5.Name = "label5";
+            label5.Size = new Size(77, 40);
+            label5.TabIndex = 17;
+            label5.Text = "Ruta del \r\nRespaldo:";
+            // 
+            // txtRutaRespaldo
+            // 
+            txtRutaRespaldo.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
+            txtRutaRespaldo.Location = new Point(114, 77);
+            txtRutaRespaldo.Name = "txtRutaRespaldo";
+            txtRutaRespaldo.Size = new Size(394, 27);
+            txtRutaRespaldo.TabIndex = 18;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
+            label4.ForeColor = Color.Black;
+            label4.Location = new Point(12, 19);
+            label4.Name = "label4";
+            label4.Size = new Size(96, 40);
+            label4.TabIndex = 16;
+            label4.Text = "Nombre del \r\nRespaldo:";
+            // 
             // txtRestauracion
             // 
             txtRestauracion.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
-            txtRestauracion.Location = new Point(202, 28);
+            txtRestauracion.Location = new Point(114, 22);
             txtRestauracion.Name = "txtRestauracion";
-            txtRestauracion.Size = new Size(338, 27);
+            txtRestauracion.Size = new Size(394, 27);
             txtRestauracion.TabIndex = 16;
-            // 
-            // btnSeleccionar
-            // 
-            btnSeleccionar.BackColor = SystemColors.ControlLight;
-            btnSeleccionar.Font = new Font("Segoe UI Black", 11F, FontStyle.Bold);
-            btnSeleccionar.ForeColor = Color.FromArgb(69, 69, 210);
-            btnSeleccionar.Location = new Point(6, 22);
-            btnSeleccionar.Name = "btnSeleccionar";
-            btnSeleccionar.Size = new Size(190, 38);
-            btnSeleccionar.TabIndex = 16;
-            btnSeleccionar.Text = "SELECCIONAR ARCHIVO";
-            btnSeleccionar.UseVisualStyleBackColor = false;
             // 
             // btnRestaurar
             // 
             btnRestaurar.BackColor = SystemColors.ControlLight;
             btnRestaurar.Font = new Font("Segoe UI Black", 11F, FontStyle.Bold);
             btnRestaurar.ForeColor = Color.FromArgb(69, 69, 210);
-            btnRestaurar.Location = new Point(392, 65);
+            btnRestaurar.Location = new Point(522, 26);
             btnRestaurar.Name = "btnRestaurar";
-            btnRestaurar.Size = new Size(148, 38);
+            btnRestaurar.Size = new Size(148, 50);
             btnRestaurar.TabIndex = 15;
             btnRestaurar.Text = "💾RESTAURAR";
             btnRestaurar.UseVisualStyleBackColor = false;
+            btnRestaurar.Click += btnRestaurar_Click;
             // 
             // label3
             // 
@@ -177,28 +225,23 @@
             label3.Size = new Size(0, 20);
             label3.TabIndex = 4;
             // 
-            // dataGridView1
+            // dgvRespaldo
             // 
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Column1 });
-            dataGridView1.Location = new Point(6, 22);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.Size = new Size(933, 215);
-            dataGridView1.TabIndex = 104;
-            // 
-            // Column1
-            // 
-            Column1.HeaderText = "Nombre del Respaldo";
-            Column1.Name = "Column1";
-            Column1.Width = 890;
+            dgvRespaldo.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvRespaldo.Columns.AddRange(new DataGridViewColumn[] { Column1, Column3, Column2 });
+            dgvRespaldo.Location = new Point(6, 22);
+            dgvRespaldo.Name = "dgvRespaldo";
+            dgvRespaldo.Size = new Size(1152, 215);
+            dgvRespaldo.TabIndex = 104;
+            dgvRespaldo.CellContentDoubleClick += dgvRespaldo_CellContentDoubleClick;
             // 
             // groupBox3
             // 
             groupBox3.BackColor = SystemColors.Control;
-            groupBox3.Controls.Add(dataGridView1);
+            groupBox3.Controls.Add(dgvRespaldo);
             groupBox3.Location = new Point(12, 264);
             groupBox3.Name = "groupBox3";
-            groupBox3.Size = new Size(945, 251);
+            groupBox3.Size = new Size(1164, 251);
             groupBox3.TabIndex = 105;
             groupBox3.TabStop = false;
             groupBox3.Text = "Respaldos Existentes";
@@ -244,19 +287,37 @@
             button7.BackColor = SystemColors.ButtonFace;
             button7.Font = new Font("Segoe UI Black", 11F, FontStyle.Bold);
             button7.ForeColor = Color.FromArgb(69, 69, 210);
-            button7.Location = new Point(890, 14);
+            button7.Location = new Point(1103, 11);
             button7.Name = "button7";
             button7.Size = new Size(73, 35);
             button7.TabIndex = 107;
             button7.Text = "AYUDA";
             button7.UseVisualStyleBackColor = false;
             // 
+            // Column1
+            // 
+            Column1.HeaderText = "Nombre del Respaldo";
+            Column1.Name = "Column1";
+            Column1.Width = 400;
+            // 
+            // Column3
+            // 
+            Column3.HeaderText = "Ruta del Respaldo";
+            Column3.Name = "Column3";
+            Column3.Width = 420;
+            // 
+            // Column2
+            // 
+            Column2.HeaderText = "Fecha de creación";
+            Column2.Name = "Column2";
+            Column2.Width = 300;
+            // 
             // Mantenimiento
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.Gainsboro;
-            ClientSize = new Size(975, 527);
+            ClientSize = new Size(1188, 527);
             Controls.Add(button7);
             Controls.Add(groupBox4);
             Controls.Add(groupBox3);
@@ -271,7 +332,7 @@
             groupBox1.PerformLayout();
             groupBox2.ResumeLayout(false);
             groupBox2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dgvRespaldo).EndInit();
             groupBox3.ResumeLayout(false);
             groupBox4.ResumeLayout(false);
             ResumeLayout(false);
@@ -287,16 +348,22 @@
         private TextBox txtNombreRespaldo;
         private Button btnRespaldo;
         private GroupBox groupBox2;
-        private Button btnSeleccionar;
         private Button btnRestaurar;
         private Label label3;
         private TextBox txtRestauracion;
-        private DataGridView dataGridView1;
+        private DataGridView dgvRespaldo;
         private GroupBox groupBox3;
         private GroupBox groupBox4;
         private Button btnEliminar;
         private Button btnDescargar;
-        private DataGridViewTextBoxColumn Column1;
         private Button button7;
+        private Label label6;
+        private TextBox txtFechaRespaldo;
+        private Label label5;
+        private TextBox txtRutaRespaldo;
+        private Label label4;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Column3;
+        private DataGridViewTextBoxColumn Column2;
     }
 }
--- SISTEMA/capa_de_presentacion/Mantenimiento.cs
+++ SISTEMA/capa_de_presentacion/Mantenimiento.cs
@@ -17,8 +17,12 @@ namespace SISTEMA
         public Mantenimiento()
         {
             InitializeComponent();
-            txtNombreRespaldo.Text = "AlmacenMiFavoritaBD_" + DateTime.Now.ToString("yyyyMMdd") + DateTime.Now.ToString("HHmmss");
+            txtNombreRespaldo.Text = "AlmacenMiFavoritaBD1_" + DateTime.Now.ToString("yyyyMMdd") + "_" + DateTime.Now.ToString("HHmmss");
             txtNombreRespaldo.Enabled = false;
+            txtRestauracion.Enabled = false;
+            txtRutaRespaldo.Enabled = false;
+            txtRestauracion.Enabled = false;
+            txtFechaRespaldo.Enabled = false;
         }
 
         private void groupBox2_Enter(object sender, EventArgs e)
@@ -34,8 +38,8 @@ namespace SISTEMA
             using (SaveFileDialog sfd = new SaveFileDialog())
             {
                 //filtrar para que solo se guarden archivos con extension backup 
-                sfd.Filter = "Backup files (*.backup)|*.backup|All files (*.*)|*.*";
-                sfd.FileName = nombreRespaldo + ".backup";
+                sfd.Filter = "Backup files (*.dump)|*.dump|All files (*.*)|*.*";
+                sfd.FileName = nombreRespaldo + ".dump";
                 sfd.Title = "Guardar respaldo de la base de datos";
 
                 //si se cancela, no hace nada
@@ -60,8 +64,9 @@ namespace SISTEMA
                     // Nombre del ejecutable pg_dump
                     string pgDumpExe = "pg_dump";
 
-                    // Argumentos recomendados: formato custom para permitir restauración con pg_restore
-                    string arguments = $"--host \"{host}\" --port {port} --username \"{username}\" --format custom --blobs --verbose --file \"{destino}\" \"{database}\"";
+                    // Argumentos en formato custom para permitir restauración con pg_restore
+                    //string arguments = $"--host \"{host}\" --port {port} --username \"{username}\" --format custom --blobs --verbose --file \"{destino}\" \"{database}\"";
+                    string arguments = $"-h {host} -p {port} -U {username} -F c -b -v -f \"{destino}\" {database}";
 
                     //se configura para ejecutar pg_dump con los argumentos especificados anteriormetne
                     var psi = new ProcessStartInfo(pgDumpExe, arguments)
@@ -87,7 +92,8 @@ namespace SISTEMA
 
                         if (process.ExitCode == 0)
                         {
-                            MessageBox.Show("Respaldo creado correctamente:\n" + destino, "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            MessageBox.Show("Respaldo creado correctamente en:\n" + destino, "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            dgvRespaldo.Rows.Add(nombreRespaldo, destino, DateTime.Now.ToString("el dd/MM/yyyy  hh:mm:ss"));
                         }
                         else
                         {
@@ -101,6 +107,124 @@ namespace SISTEMA
                     MessageBox.Show("Error al generar el respaldo: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
             }
+        }
+
+        private void btnRestaurar_Click(object sender, EventArgs e)
+        {
+            string rutaRestaurar = dgvRespaldo.CurrentRow?.Cells[1].Value?.ToString();
+
+            if (string.IsNullOrEmpty(rutaRestaurar))
+            {
+                MessageBox.Show("Seleccione un respaldo existente para restaurar.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Validar que el archivo existe
+            if (!System.IO.File.Exists(rutaRestaurar))
+            {
+                MessageBox.Show("El archivo de respaldo no existe en la ruta especificada.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            try
+            {
+                // Obtener parámetros de conexión desde la clase Conexion
+                var conexion = new Conexion();
+                var builder = new NpgsqlConnectionStringBuilder(conexion.con);
+
+                //guarda los datos de conexion 
+                string host = builder.Host;
+                int port = builder.Port;
+                string username = builder.Username;
+                string password = builder.Password;
+                string database = builder.Database;
+
+                //crear la base de datos donde se hará la restauración
+                string createdbExe = "createdb";
+                string createdbArgs = $"-h {host} -p {port} -U {username} {database + "_restaurada"}";
+
+                var createdbPsi = new ProcessStartInfo(createdbExe, createdbArgs)
+                {
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    CreateNoWindow = true
+                };
+
+                //pasar contraseña para no terner que escribirla
+                createdbPsi.Environment["PGPASSWORD"] = password;
+
+                //empieza el proceso de crear la base de datos
+                using (var createdbProcess = Process.Start(createdbPsi))
+                {
+                    //capturar errores de ejecucion
+                    string createdbErr = createdbProcess.StandardError.ReadToEnd();
+                    createdbProcess.WaitForExit();
+
+                    // Si el código es 0 o la BD ya existe, continua
+                    if (createdbProcess.ExitCode != 0 && !createdbErr.Contains("already exists"))
+                    {
+                        MessageBox.Show($"Error al crear base de datos:\n{createdbErr}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                    MessageBox.Show("Base de datos para restauración creada. Empezando a copiar el contenido...", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+                // Nombre del ejecutable pg_restore
+                string pgRestoreExe = "pg_restore";
+
+                // Argumentos en formato custom para permitir restauración con pg_restore
+                //string arguments = $"--host \"{host}\" --port {port} --username \"{username}\" --format custom --verbose --file \"{rutaRestaurar}\" \"{database}\"";
+                //string arguments = $"--host \"{host}\" --port {port} --username svc_restore -n admin -n public --clean --if-exists --no-owner --no-comments --no-acl --exit-on-error --file \"{rutaRestaurar}\"";
+                //string arguments = $"-h {host} -p {port} -U {username} -d {database} -v --clean --if-exists --no-owner --no-comments --no-acl \"{rutaRestaurar}\"";
+                string arguments = $"-h {host} -p {port} -U {username} -d {database + "_restaurada"} -v -F c --clean --if-exists --no-owner --no-comments --no-acl \"{rutaRestaurar}\"";
+
+                //se configura para ejecutar pg_restore con los argumentos especificados anteriormetne
+                var psi = new ProcessStartInfo(pgRestoreExe, arguments)
+                {
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    CreateNoWindow = true
+                };
+
+                // Evitar que pg_restore pida contraseña interactiva
+                psi.Environment["PGPASSWORD"] = password;
+
+                //empieza el proceso de pg_restore
+                using (var process = Process.Start(psi))
+                {
+                    // Leer salidas para diagnóstico 
+                    string stdOut = process.StandardOutput.ReadToEnd();
+                    string stdErr = process.StandardError.ReadToEnd();
+
+                    //esperar a que el proceso termine para mostrar si el respaldo fue exitoso o no
+                    process.WaitForExit();
+
+                    if (process.ExitCode == 0)
+                    {
+                        MessageBox.Show("Restauración creada correctamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);                        
+                    }
+                    else
+                    {
+                        string mensaje = $"pg_restore finalizó con código {process.ExitCode}.\n\nError: {stdErr}\n\nSalida: {stdOut}";
+                        MessageBox.Show(mensaje, "Error al restaurar base de datos", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al restaurar base de datos: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void dgvRespaldo_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0) return;
+
+            txtRestauracion.Text = dgvRespaldo.Rows[e.RowIndex].Cells[0].Value?.ToString();
+            txtRutaRespaldo.Text = dgvRespaldo.Rows[e.RowIndex].Cells[1].Value?.ToString();
+            txtFechaRespaldo.Text = dgvRespaldo.Rows[e.RowIndex].Cells[2].Value?.ToString();
         }
     }
 }
